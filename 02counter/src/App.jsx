c395d5dac45f733a2d5cb1192d0fb7e0ada3cb5a
targetlakshya <thetarget018@gmail.com>
2024-02-11 12:30:16 +0530

    Counter App Done

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,34 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  //  Hooks useState - > State Change in the DOM
+  const [counter, setCounter] = useState(0) 
+
+  // let counter = 5
+
+  const addValue = () => {
+    if(counter < 20) { 
+      setCounter(counter+1)
+    }   
+  };
+  
+  const decreasevalue = () => {
+    if (counter > 0) {
+      setCounter(counter-1)
+    }
+  }
+  return (
+    <>
+      <h1>Counter App</h1>
+      <h2>Counter Value : {counter}</h2>
+      <button id='addvalue' onClick={addValue}>Increase Value </button> <br />
+      <button onClick={decreasevalue}>Decrease Value</button>
+    </>
+  )
+}
+
+export default App
