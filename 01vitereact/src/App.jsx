5b7b8aefe87ab6ddfc9cf5dbed977bcadff0d5d8
targetlakshya <thetarget018@gmail.com>
2024-02-11 11:18:33 +0530

    Evaluated Expression

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,9 +1,14 @@
 import Chai from "./chai"
 function App() {
-
+  const username = "Lakshya"
 
   return (
+    <>
    <Chai />
+   <h1>Chai or React {username}  </h1> 
+   {/* Evaluated Expression {} */}
+   <p>test page</p>
+   </>
   )
 }
 
--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -2,8 +2,34 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+// function MyApp() {
+//   return (
+//     <div>
+//       <h1>Custom App !</h1>
+//     </div>
+//   )
+// }
+
+// const anotherElement = (
+//   <a href="https://google.com" target='_blank'>Visit Google</a>
+// )
+
+const anotherUser = " Chai or React JS"
+
+// how to create an react element
+
+const reactElement = React.createElement(
+  'a', // Define element
+  {
+    href: 'https://google.com', target: '_blank'
+  },
+  'CLick me to visit google',
+  anotherUser
+)
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    // <App />
+    // anotherElement  
+    reactElement
+  
 )
