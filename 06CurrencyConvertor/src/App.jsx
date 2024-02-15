dee0bc2fd24164e6cd10fbb2e24940877ceb7d14
targetlakshya <thetarget018@gmail.com>
2024-02-15 11:12:37 +0530

    API fetch

--- 06CurrencyConvertor/src/App.jsx
+++ 06CurrencyConvertor/src/App.jsx
@@ -1,14 +1,13 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
+
+  
   return (
     <>
-     
+     <h1 className='text-3xl bg-orange-500'>Currency App </h1>
     </>
   )
 }
