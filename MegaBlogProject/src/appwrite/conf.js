2681836044080f22ad84681c63ba15c64dfcf2f2
targetlakshya <thetarget018@gmail.com>
2024-02-18 20:53:27 +0530

    package.json updated

--- MegaBlogProject/src/appwrite/conf.js
+++ MegaBlogProject/src/appwrite/conf.js
@@ -61,11 +61,75 @@ export class Service{
                 conf.appwriteCollectionId,
                 slug
             )
+            return true
         } catch (error) {
             console.log("Appwrite service :: createPost :: error :: ", error)
-            
+            return false
         }
     }
+
+    async getPost(slug) {
+        try {
+            return await this.databases.getDocument(
+                conf.appwriteDatabaseId,
+                conf.appwriteCollectionId,
+                slug,
+            )
+        } catch (error) {
+            console.log("Appwrite service :: createPost :: error :: ", error)
+            return false
+        }
+    }
+
+    async getposts(queries = [Query.equal("status", "active")]){
+        try {
+            return await this.databases.listDocuments(
+                conf.appwriteDatabaseId,
+                conf.appwriteCollectionId,
+                queries,
+            )
+        } catch (error) {
+            console.log("Appwrite service :: createPost :: error :: ", error)
+            return false
+        }
+    }
+
+    // File Upload Service
+
+    async uploadFile(file){
+        try {
+            return await this.bucket.createFile(
+                conf.appwriteBucketId,
+                ID.unique(),
+                file,
+            )
+        } catch (error) {
+            console.log("Appwrite service :: uploadFile :: error :: ", error)
+            return false
+        }
+    }
+
+    async deleteFile(fileId){
+        try {
+            return await this.bucket.deleteFile(
+                conf.appwriteBucketId,
+                fileId
+            )
+            return true
+        } catch (error) {
+            console.log("Appwrite service :: deleteFile :: error :: ", error)
+            return false
+        }
+    }
+
+    getFilePreview(fileId){
+        return this.bucket.getFilePreview(
+            conf.appwriteBucketId,
+            fileId,
+        )
+    }
+
+    
 } 
 
 const service = new Service()
