4b57cb949d5914452210d8461895078c8762ea04
targetlakshya <thetarget018@gmail.com>
2024-02-18 10:34:50 +0530

    Todo List Complted

--- 10TodoListApp/src/Contexts/TodoContext.js
+++ 10TodoListApp/src/Contexts/TodoContext.js
@@ -1,27 +1,19 @@
-import { createContext, useContext } from 'react';
+import {createContext, useContext} from "react"
 
 export const TodoContext = createContext({
     todos: [
         {
             id: 1,
-            todo : "Todo Message",
-            completed : false,
-
-        } , 
+            todo: " Todo msg",
+            completed: false,
+        }
     ],
-    addTodo : (todo) => {
-
-    },
-    updatedTodo : (id, todo) => {
-
-    },
-    deleteTodo: (id) => {
-
-    },      
-    toggleComplete: (id) => {
-        
-    } 
+    addTodo: (todo) => {},
+    updateTodo: (id, todo) => {},
+    deleteTodo: (id) => {},
+    toggleComplete: (id) => {}
 })
+
 
 export const useTodo = () => {
     return useContext(TodoContext)
